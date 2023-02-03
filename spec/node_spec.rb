10f051558d0ce6b22e2e0dfb439a4d5a9d235192
Katie Lonsdale <katie952@gmail.com>
2023-02-02 20:12:40 -0700

    test: add test for node

---
+++ spec/node_spec.rb
@@ -0,0 +1,24 @@
+require './lib/node'
+require 'rspec'
+
+RSpec.describe Node do
+  describe '#initialize' do
+    it 'exists' do
+      node = Node.new('plop')
+
+      expect(node).to be_instance_of(Node)
+    end
+
+    it 'has a sound' do
+      node = Node.new('plop')
+
+      expect(node.data).to eq('plop')
+    end
+
+    it 'has no next node at creation' do
+      node = Node.new('plop')
+
+      expect(node.next_node).to eq(nil)
+    end
+  end
+end
