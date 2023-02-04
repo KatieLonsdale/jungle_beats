90bebcf0fe23c7843ac3579a987fa014e8b274d1
Katie Lonsdale <katie952@gmail.com>
2023-02-03 19:33:30 -0700

    test: add includes tests

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -111,32 +111,47 @@ RSpec.describe LinkedList do
     end
 
     it 'inserts a node at a given position' do
-        list = LinkedList.new
-        
-        list.append('plop')
-        list.append('suu')
-        list.prepend('dop')
-        list.insert(1, "woo")
+      list = LinkedList.new
+      
+      list.append('plop')
+      list.append('suu')
+      list.prepend('dop')
+      list.insert(1, "woo")
 
-        expect(list.count).to eq 4
-        expect(list.to_string).to eq('dop woo plop suu')
-        expect(list.head.next_node.data).to eq('woo')
-        expect(list.head.next_node.next_node.data).to eq('plop')
+      expect(list.count).to eq 4
+      expect(list.to_string).to eq('dop woo plop suu')
+      expect(list.head.next_node.data).to eq('woo')
+      expect(list.head.next_node.next_node.data).to eq('plop')
     end
 
     it 'can find x elements at index positions' do
       list = LinkedList.new
         
-        list.append('deep')
-        list.append('woo')
-        list.append('shi')
-        list.append('shu')
-        list.append('blop')
+      list.append('deep')
+      list.append('woo')
+      list.append('shi')
+      list.append('shu')
+      list.append('blop')
 
-        expect(list.find(2, 1)).to eq('shi')
-        expect(list.find(1, 3)).to eq('woo shi shu')
-        expect(list.find(7, 1)).to eq(nil)
+      expect(list.find(2, 1)).to eq('shi')
+      expect(list.find(1, 3)).to eq('woo shi shu')
+      expect(list.find(7, 1)).to eq(nil)
     end
+
+    it 'can search for a sound' do
+      list = LinkedList.new
+        
+      list.append('deep')
+      list.append('woo')
+      list.append('shi')
+      list.append('shu')
+      list.append('blop')
+
+      expect(list.includes?('deep')).to be true 
+      expect(list.includes?('dep')).to be false 
+    end
+
+    # it 'can remove the last element' do
   end
 
 
