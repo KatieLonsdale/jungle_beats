88451d236ddbe79320fe3f8d647c08e35a609ebb
Katie Lonsdale <katie952@gmail.com>
2023-02-04 12:00:52 -0700

    feat: add append method

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -5,5 +5,21 @@ class JungleBeat
   def initialize
     @list = LinkedList.new
   end
-  
+
+  def append(sound_string)
+    sound = ''
+    sound_string.each_char do |character|
+      if character == ' '
+        node = Node.new(sound)
+        @list.append(node.data)
+        sound = ''
+      else
+        sound = sound.concat("#{character}")
+      end
+    end
+    node = Node.new(sound)
+    @list.append(node.data)
+    list.to_string
+  end
+
 end
