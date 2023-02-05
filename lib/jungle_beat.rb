f1556d5513fc3be087073b680aa4a54b741178fc
Katie Lonsdale <katie952@gmail.com>
2023-02-05 14:05:27 -0700

    refactor: remove unnecessary code

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,5 +1,4 @@
 class JungleBeat
-
   attr_reader :list
 
   def initialize
@@ -8,21 +7,16 @@ class JungleBeat
 
   def append(sound_string)
     sound = ''
-    return_value = ''
     sound_string.each_char do |character|
       if character == ' '
-        node = Node.new(sound)
-        @list.append(node.data)
+        @list.append(sound)
         sound = ''
-        return_value = return_value.concat("#{character}")
       else
         sound = sound.concat("#{character}")
-        return_value = return_value.concat("#{character}")
       end
     end
-    node = Node.new(sound)
-    @list.append(node.data)
-    return_value
+    @list.append(sound)
+    sound_string
   end
 
   def count
